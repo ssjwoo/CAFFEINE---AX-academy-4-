59ca25e629f93b0dd56d3fd0979a581d100eb18f
HosikYOON <00ghtlr00@naver.com>
2025-12-18 12:53:43 +0900

    admin 에러 개선

--- 21_frontend_admin/next.config.ts
+++ 21_frontend_admin/next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // output: "export",   // ★ 정적 사이트 Export 활성화 (API Routes 사용 시 주석 처리 필요)
+  output: "export",
 };
 
 export default nextConfig;
